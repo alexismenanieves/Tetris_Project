1da3b79850dda956ed5cf9a91182a74de16b5e93
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-23 20:48:31 -0500

    Added texture to sprite

--- Tetris_Main.cpp
+++ Tetris_Main.cpp
@@ -3,7 +3,17 @@
 using namespace sf;
 int main()
 {
+  // First we have to create a window object, sized 400 x 600
   RenderWindow window(VideoMode(400,600),"Tetris - A homemade game");
+  // Now let's load a texture, that is an image we will move
+  Texture t;
+  t.loadFromFile("Images/tiles.png");
+  // The loaded texture is converted to a sprite object
+  Sprite s(t);
+  // And we define a rectangle where to crop the image
+  s.setTextureRect(IntRect(0,0,18,18));
+  
+  // Ok, let's add an instruction to poll if the window is open
   while(window.isOpen())
   {
     Event e;
@@ -12,7 +22,11 @@ int main()
       if(e.type == Event::Closed)
         window.close();
     }
+  // Now clear the window
   window.clear(Color::White);
+  // Let's draw our sprite into the window
+  window.draw(s);
+  // After drawing the sprite, let's display the window
   window.display();
   }
   return 0;
