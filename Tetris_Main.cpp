281cd971b1f54f6a5d35a21c8e9299c171641e40
alexismenanieves <alexismenanieves@hotmail.com>
2021-05-23 21:12:20 -0500

    Added structures

--- Tetris_Main.cpp
+++ Tetris_Main.cpp
@@ -1,6 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include <time.h>
 using namespace sf;
+
+// Let's define a field array
+const int M = 20;
+const int N = 10;
+int field[M][N] = {0};
+
+struct Point
+{int x,y;} a[4], b[4];
+
+// We need the various forms of Tetris figures
+int figures[7][4] = 
+{
+  1,3,5,7, // I
+  2,4,5,7, // Z
+  3,5,4,6, // S
+  3,5,4,7, // T
+  2,3,5,7, // L
+  3,5,7,6, // J
+  2,3,4,5, // O
+};
+
 int main()
 {
   // First we have to create a window object, sized 400 x 600
@@ -22,8 +43,21 @@ int main()
       if(e.type == Event::Closed)
         window.close();
     }
+    
+  int n=3;
+  for(int i=0;i<4;i++)
+  {
+    a[i].x = figures[n][i] % 2;
+    a[i].y = figures[n][i] / 2;
+  }
   // Now clear the window
   window.clear(Color::White);
+  
+  for(int i=0; i<4; i++)
+  {
+    s.setPosition(a[i].x*18, a[i].y*18);
+    window.draw(s);
+  }
   // Let's draw our sprite into the window
   window.draw(s);
   // After drawing the sprite, let's display the window
